2629e6644390b97b91fdf6fd55922d8c9384dba3
Skip Gibson <skipcloudgibson@gmail.com>
2024-10-09 10:45:45 +0100

    infra: add env var for ecr region

--- infrastructure/modules/basic/circleci.tf
+++ infrastructure/modules/basic/circleci.tf
@@ -11,3 +11,10 @@ resource "circleci_envvar" "oidc_role" {
   variable_name  = "OIDC_ROLE_ARN"
   variable_value = aws_iam_role.circleci_oidc.arn
 }
+
+resource "circleci_envvar" "aws_ecr_repo" {
+  vcs_provider   = "github"
+  project_name   = local.circleci_project_name
+  variable_name  = "ECR_AWS_REGION"
+  variable_value = "us-east-1"
+}
